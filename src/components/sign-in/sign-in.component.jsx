89ed5869fc6c6e1ea583141d4b1250f3a5b1f5d0
Dannus90 <61561009+Dannus90@users.noreply.github.com>
2020-05-23 14:23:27 +0200

    integrated firebase into app, signinpage with component and signin with google

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
+import { signInWithGoogle } from "../../firebase/firebase.utils";
 
 import "./sign-in.styles.scss";
 
@@ -49,10 +50,12 @@ class SignIn extends Component {
                         label="password"
                         handleChange={this.handleChange}
                     />
-
-                    <CustomButton type="submit" value="SubmitForm">
-                        Sign In
-                    </CustomButton>
+                    <div className="buttons">
+                        <CustomButton type="submit">Sign In</CustomButton>
+                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+                            Sign in with google
+                        </CustomButton>
+                    </div>
                 </form>
             </div>
         );
---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,27 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const config = {
+    apiKey: "AIzaSyDAWfRN-hiym4nqbzwfNoL5YqPiD0w9s-I",
+    authDomain: "e-commerce-db-16e78.firebaseapp.com",
+    databaseURL: "https://e-commerce-db-16e78.firebaseio.com",
+    projectId: "e-commerce-db-16e78",
+    storageBucket: "e-commerce-db-16e78.appspot.com",
+    messagingSenderId: "45839024734",
+    appId: "1:45839024734:web:29af70f2b0db2845deeacf",
+    measurementId: "G-LRVDZZZE11",
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+//Google auth
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: "select_account" });
+
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
