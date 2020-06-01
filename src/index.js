a76e539c169d9cc138419671dca4901f195241ae
Dannus90 <61561009+Dannus90@users.noreply.github.com>
2020-06-01 14:59:42 +0200

    moved our directory, shop data into its own reducer, created respective selectors and updated corresponding components with tredux folow for directory/shop

--- src/index.js
+++ src/index.js
@@ -4,13 +4,17 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import store from "./redux/store";
+import { store, persistor } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
 
+//persistor is the persistant version of our store.
 ReactDOM.render(
     <React.StrictMode>
         <Provider store={store}>
             <BrowserRouter>
-                <App />
+                <PersistGate persistor={persistor}>
+                    <App />
+                </PersistGate>
             </BrowserRouter>
         </Provider>
     </React.StrictMode>,
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,8 +1,24 @@
 import { combineReducers } from "redux";
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; //Local storage on our window browser. Works also to import sessionStorage - difference in functionality though.
+import directoryReducer from "./directory/directory.reducer";
+import shopReducer from "./shop/shop.reducer";
 
-export default combineReducers({
+//we start storing form the root. whitelist is a string name of any reducers that we want to store.storage is just localstorage
+const persistConfig = {
+    key: "root",
+    storage,
+    whitelist: ["cart"],
+};
+
+const rootReducer = combineReducers({
     user: userReducer,
     cart: cartReducer,
+    directory: directoryReducer,
+    shop: shopReducer,
 });
+
+//user already stored by firebase Auth. Modified version of our root reducer but not with modification capabilities.
+export default persistReducer(persistConfig, rootReducer);
